8ae8c698a6603e750514d083712d42abb63a8750
koffmann81 <66971880+koffmann81@users.noreply.github.com>
2023-04-20 13:02:02 +0200

    Update rNumber.py

--- rNumber.py
+++ rNumber.py
@@ -2,20 +2,21 @@ import streamlit as st
 import random
 
 def main():
-    st.title("Random Number Generator")
-    st.write("Select the range of numbers to generate a random integer from:")
+    st.title("Tilfeldig tall")
+    st.write("Velg laveste og høyeste tall:")
 
     # Get user input for the range of random numbers
-    start_num = st.number_input("Start number", value=0)
-    end_num = st.number_input("End number", value=10)
+    start_num = st.number_input("Nedre grense", value=0)
+    end_num = st.number_input("Øvre grense", value=10)
 
     # Check if start_num is less than end_num
     if start_num >= end_num:
-        st.error("Error: Start number should be less than end number.")
+        st.error("Error: Nedre grense må være lavere enn øvre grense.")
     else:
         # Generate and display the random integer
         random_num = random.randint(start_num, end_num)
-        st.write(f"Random integer between {start_num} and {end_num}: {random_num}")
+        st.write(f"Tilfeldig tall mellom {start_num} and {end_num}: ")
+        st.title(random_num)
 
 if __name__ == "__main__":
     main()
